ac1a84524f3cb755278d06ba0fafb1e77a40b260
Lam Pham <lamcaro12212332@gmail.com>
2025-06-20 20:46:16 +0700

    Raw Project

---
+++ filmsverts/src/main/java/com/example/filmsverts/FilmsvertsApplication.java
@@ -0,0 +1,13 @@
+package com.example.filmsverts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsvertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmsvertsApplication.class, args);
+	}
+
+}
---
+++ filmsverts/src/test/java/com/example/filmsverts/FilmsvertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filmsverts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmsvertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
